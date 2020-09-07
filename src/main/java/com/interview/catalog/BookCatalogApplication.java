ca434c9c755c0575e9d15a0f892cea0cae4fe5f6
anil <anil>
2020-09-07 10:38:02 +1000

    BookCatalog service - made changes for Ribbon load balancer

--- src/main/java/com/interview/catalog/BookCatalogApplication.java
+++ src/main/java/com/interview/catalog/BookCatalogApplication.java
@@ -1,14 +1,17 @@
 package com.interview.catalog;
 
+import com.interview.catalog.config.RibbonConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClients;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableCircuitBreaker
+@RibbonClients(defaultConfiguration = RibbonConfiguration.class)
 public class BookCatalogApplication {
 
     public static void main(String[] args) {
